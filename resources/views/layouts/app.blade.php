9f3e753c38b8e7f6901a42c5332d7242ed150a3f
Michal Nylec <michalnylec@gmail.com>
2021-12-10 14:19:36 +0000

    bootstrap progress.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+
 <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
 
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
@@ -10,14 +13,16 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Quacker - @yield('title') </title>
 </head>
+
 <body>
 
 <img src="{{ asset('/img/quacker-logo-1.png') }}" alt="Quacker Logo" height="200">
 
 <h1>Quacker - @yield('title')</h1>
 
-    <div>
-        @yield('content')
-    </div>
+{{--@include('includes.navbar')--}}
+<div>
+    @yield('content')
+</div>
 </body>
 </html>
