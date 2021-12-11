a5eb5177e3980df0996b96d45ebb11f6005d2f92
Michal Nylec <michalnylec@gmail.com>
2021-12-11 14:50:51 +0000

    post edit view started

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -3,5 +3,17 @@
 @section('title', 'Edit Post')
 
 @section('content')
+    {{--todo modify so it edits item--}}
+    <form method="POST" action="{{ route('posts.store') }}">
+        @csrf
+        <p>Title: <input type="text" name="title" value="{{$post->title}}"></p>
+        <label>
+            <textarea name="text_content"
+                      style="width:250px;height:150px;resize:none;">{{$post->text_content}}</textarea>
+        </label>
+        <br>
+        <input type="submit" value="Submit">
+    </form>
+
     edit
 @endsection
