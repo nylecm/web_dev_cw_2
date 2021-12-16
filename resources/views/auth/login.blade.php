9d77bf05f774782f8739291173f42fbba244d1dc
Michal Nylec <michalnylec@gmail.com>
2021-12-16 15:31:54 +0000

    log in and register form redesign

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,69 +1,74 @@
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+@extends('layouts.app')
 
+@section('title', 'Login')
 
-<div class="container mt-5">
-    <div class="d-flex flex-column">
-        <x-guest-layout>
-            <x-auth-card>
-                <x-slot name="logo">
-                    <a href="/">
-                        <x-application-logo class="w-20 h-20 fill-current text-gray-500"/>
-                    </a>
-                </x-slot>
+@section('content')
 
-                <!-- Session Status -->
-                <x-auth-session-status class="mb-4" :status="session('status')"/>
+    <div class="container mt-5">
+        <div class="d-flex flex-column">
+            <x-guest-layout>
+                    <div class="container mt-5">
+                        <div class="d-flex flex-column" id="post_view">
+                            <div class="card">
+                                <div class="card-body">
 
-                <!-- Validation Errors -->
-                <x-auth-validation-errors class="mb-4" :errors="$errors"/>
+                                    <!-- Session Status -->
+                                    <x-auth-session-status class="mb-4" :status="session('status')"/>
 
-                <form method="POST" action="{{ route('login') }}">
-                @csrf
+                                    <!-- Validation Errors -->
+                                    <x-auth-validation-errors class="mb-4" :errors="$errors"/>
 
-                <!-- Email Address -->
-                    <div>
-                        <x-label for="email" :value="__('Email')"/>
+                                    <form method="POST" action="{{ route('login') }}">
+                                    @csrf
 
-                        <x-input id="email" class="form-control" type="email" name="email" :value="old('email')"
-                                 required autofocus/>
+                                    <!-- Email Address -->
+                                        <div>
+                                            <x-label for="email" :value="__('Email')"/>
+
+                                            <x-input id="email" class="form-control" type="email" name="email"
+                                                     :value="old('email')"
+                                                     required autofocus/>
+                                        </div>
+
+                                        <!-- Password -->
+                                        <div class="mt-4">
+                                            <x-label for="password" :value="__('Password')"/>
+
+                                            <x-input id="password" class="form-control"
+                                                     type="password"
+                                                     name="password"
+                                                     required autocomplete="current-password"/>
+                                        </div>
+
+                                        <!-- Remember Me -->
+                                        <div class="block mt-4">
+                                            <label for="remember_me" class="inline-flex items-center">
+                                                <input id="remember_me" type="checkbox"
+                                                       class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                                       name="remember">
+                                                <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                                            </label>
+                                        </div>
+
+                                        <div class="flex items-center justify-end mt-4">
+                                            @if (Route::has('password.request'))
+                                                <a class="underline text-sm text-gray-600 hover:text-gray-900"
+                                                   href="{{ route('password.request') }}">
+                                                    {{ __('Forgot your password?') }}
+                                                </a>
+                                            @endif
+
+                                            <x-button class="btn btn-primary">
+                                                {{ __('Log in') }}
+                                            </x-button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-
-                    <!-- Password -->
-                    <div class="mt-4">
-                        <x-label for="password" :value="__('Password')"/>
-
-                        <x-input id="password" class="form-control"
-                                 type="password"
-                                 name="password"
-                                 required autocomplete="current-password"/>
-                    </div>
-
-                    <!-- Remember Me -->
-                    <div class="block mt-4">
-                        <label for="remember_me" class="inline-flex items-center">
-                            <input id="remember_me" type="checkbox"
-                                   class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                                   name="remember">
-                            <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                        </label>
-                    </div>
-
-                    <div class="flex items-center justify-end mt-4">
-                        @if (Route::has('password.request'))
-                            <a class="underline text-sm text-gray-600 hover:text-gray-900"
-                               href="{{ route('password.request') }}">
-                                {{ __('Forgot your password?') }}
-                            </a>
-                        @endif
-
-                        <x-button class="btn btn-primary">
-                            {{ __('Log in') }}
-                        </x-button>
-                    </div>
-                </form>
-            </x-auth-card>
-        </x-guest-layout>
+            </x-guest-layout>
+        </div>
     </div>
-</div>
+
+@endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,21 +1,31 @@
 @extends('layouts.app')
 
 @section('title')
-    Registering a User
+    Register an Account
 @endsection
 
 @section('content')
-    <x-auth-validation-errors class="mb-4" :errors="$errors" />
+    <div class="container mt-5">
+        <div class="d-flex flex-column" id="post_view">
+            <div class="card">
+                <div class="card-body">
 
-    <form method="POST" action="{{ route('register')}}">
-        @csrf
-        <p>Username: <input type="text" name="user_name"></p>
-        <p>Full Name: <input type="text" name="full_name"></p>
-        <p>Email: <input type="email" name="email"></p>
-{{--        <p>Date of Birth: <input type="text" name="date_of_birth"></p>--}}
-        <p>Password: <input type="password" name="password" required autocomplete="new-password"></p>
-        <p>Confirm Password: <input type="password" name="password_confirmation" required></p>
-        <input type="submit" value="Submit">
-{{--        <a href="{{ route('welcome')}}">Cancel</a>--}}
-    </form>
+                    <x-auth-validation-errors class="mb-4" :errors="$errors"/>
+
+                    <form method="POST" action="{{ route('register')}}">
+                        @csrf
+                        <p>Username: <input class="form-control" type="text" name="user_name"></p>
+                        <p>Full Name: <input class="form-control" type="text" name="full_name"></p>
+                        <p>Email: <input class="form-control" type="email" name="email"></p>
+                        {{--        <p>Date of Birth: <input type="text" name="date_of_birth"></p>--}}
+                        <p>Password: <input class="form-control" type="password" name="password" required autocomplete="new-password"></p>
+                        <p>Confirm Password: <input class="form-control" type="password" name="password_confirmation" required></p>
+                        <input class="btn btn-primary" type="submit" value="Submit">
+                        {{--        <a href="{{ route('welcome')}}">Cancel</a>--}}
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
 @endsection
