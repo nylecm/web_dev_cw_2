4415706f3d1725c0b934458d4929bf304e63391e
Michal Nylec <michalnylec@gmail.com>
2021-12-16 15:24:31 +0000

    edit comment ui update

--- resources/views/comments/edit.blade.php
+++ resources/views/comments/edit.blade.php
@@ -5,14 +5,20 @@
 @endsection
 
 @section('content')
-    <form method="POST" action="{{ route('comments.update', $comment->id) }}">
-        @csrf
-        <label>
-            <textarea name="text_content"
-                      style="width:250px;height:150px;resize:none;">{{ $comment->text_content}}</textarea>
-        </label>
-        <br>
-        <input type="submit" value="Submit">
-    </form>
-
+    <div class="container mt-5">
+        <div class="d-flex flex-column" id="post_view">
+            <div class="card">
+                <div class="card-body">
+                    <form method="POST" action="{{ route('comments.update', $comment->id) }}">
+                        @csrf
+                        <label>
+                        <textarea class="form-control" name="text_content" style="width:250px;height:150px;resize:none;">{{ $comment->text_content}}</textarea>
+                        </label>
+                        <br>
+                        <input class="btn btn-primary" type="submit" value="Submit">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
