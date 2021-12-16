9167713419f120adb05aae7186115c6e98781047
Michal Nylec <michalnylec@gmail.com>
2021-12-16 16:42:16 +0000

    Edit profile redesign

--- resources/views/profiles/edit.blade.php
+++ resources/views/profiles/edit.blade.php
@@ -5,22 +5,31 @@
 @endsection
 
 @section('content')
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-    <form method="POST" action="{{ route('profiles.update', $profile->id) }}">
-        @csrf
-        <label>
-            <textarea name="bio"
+    <div class="container mt-5">
+        <div class="d-flex flex-column" id="post_view">
+            <div class="card">
+                <div class="card-body">
+
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+                    <form method="POST" action="{{ route('profiles.update', $profile->id) }}">
+                        @csrf
+                        <label>
+                        <textarea class="form-control" name="bio"
                       style="width:250px;height:150px;resize:none;">{{ $profile->bio}}</textarea>
-        </label>
-        <br>
-        <input type="submit" value="Submit">
-    </form>
+                        </label>
+                        <br>
+                        <input class="btn btn-primary" type="submit" value="Submit">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
